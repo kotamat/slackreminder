6da79615f232a156827de6108fe1e10c1bc5f330
kotamat <kota1861@gmail.com>
2019-08-10 22:41:21 +0900

    add yargs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
 import { WebClient } from "@slack/web-api";
+import * as yargs from "yargs";
 
-const token = process.env.SLACK_TOKEN;
+const argv = yargs.options({
+  token: {
+    type: "string",
+    alias: "t",
+    demandOption: true
+  },
+  channel: {
+    type: "string",
+    alias: "c",
+    demandOption: true
+  }
+}).argv;
+
+// const token = process.env.SLACK_TOKEN;
+// const channelId = process.env.CHANNEL_ID;
+const token = argv.token;
+const channelId = argv.channel;
+
 const web = new WebClient(token);
+
+(async (): Promise<void> => {
+  // fetch all user in this channel
+  web.channels.info({ channel: channelId });
+})();
